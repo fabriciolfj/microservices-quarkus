3bbcf7fb3e2f760fc88f5013e2fee6cdd7a9056e
fabricio <fabricio.jacob@outlook.com>
2021-09-25 21:37:26 -0300

    add

--- account/src/main/java/com/github/fabriciolfj/controller/ExtractController.java
+++ account/src/main/java/com/github/fabriciolfj/controller/ExtractController.java
@@ -12,7 +12,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
